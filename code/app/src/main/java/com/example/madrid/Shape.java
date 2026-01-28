cbe05643300f040f133daf43223a0189a7a4c5b2
Sabit Kamali Tasfi <tasfikamali@Sabits-MacBook-Air.local>
2026-01-27 19:54:02 -0700

    Changed color to red

--- code/app/src/main/java/com/example/madrid/Shape.java
+++ code/app/src/main/java/com/example/madrid/Shape.java
@@ -3,6 +3,7 @@ package com.example.madrid;
 public abstract class Shape {
     protected int x;
     protected int y;
+    String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
