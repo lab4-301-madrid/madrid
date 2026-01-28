218b5ce4c963520a906ac8a4af75cf0229556ec8
Tiolu Olu-olayioye <tiolu@Tiolus-MacBook-Pro.local>
2026-01-27 20:18:58 -0700

    change made to Shape

--- code/app/src/main/java/com/example/madrid/Shape.java
+++ code/app/src/main/java/com/example/madrid/Shape.java
@@ -3,8 +3,6 @@ package com.example.madrid;
 public abstract class Shape {
     protected int x;
     protected int y;
-    String color = "green";
-
     String color = "orange";
 
     public Shape(int x, int y) {
