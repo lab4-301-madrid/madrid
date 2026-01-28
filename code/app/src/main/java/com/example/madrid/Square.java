96e6dc78e251f257dd6b73dbfe7194124b286615
花菱小由 <honaka@hualingxiaoyoudeMacBook-Pro.local>
2026-01-27 20:17:05 -0700

    Add Square class

---
+++ code/app/src/main/java/com/example/madrid/Square.java
@@ -0,0 +1,14 @@
+package com.example.madrid;
+
+public class Square extends Shape {
+    private int side;
+
+    public Square(int x, int y, int side) {
+        super(x, y);
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
+    }
+}
